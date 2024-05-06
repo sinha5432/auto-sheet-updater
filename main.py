700fb9a6ea773b0fc18a3cdfe855109d32ed3a17
Manasvi Sinha <sinha5432>
2024-05-06 16:57:50 +0530

    extracted names and idas

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import discord
-# permission -integer: 68608
+import re
+
 # Discord bot token
 TOKEN = 'TOKEN'
 
 # ID of the server and channel you want to read messages from
-SERVER_ID = 123
-CHANNEL_ID = 123
+SERVER_ID = 'ID'
+CHANNEL_ID = 'ID'
 
 # Message to display
 DISPLAY_MESSAGE = "Hello from the bot!"
@@ -16,6 +17,9 @@ intents.messages = True  # Enable the message intent
 intents.message_content = True # Enable message texts
 client = discord.Client(intents=intents)
 
+
+
+
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
@@ -24,19 +28,45 @@ async def on_ready():
     
     channel = client.get_channel(CHANNEL_ID)
 
-    async for message in channel.history(limit=5):
+    async for message in channel.history(limit=1):
         messages.append(message)
 
     # Display another message after reading all the previous messages
     # await channel.send(DISPLAY_MESSAGE)
-
- 
-
+    
     # Print all the previous messages
 
+    guild = await client.fetch_guild(SERVER_ID)
+
+
+
     for message in messages:
-        print(f"{message.author}: {message.content}")
+        summary_message = message.content
+
+        # print(summary_message)
+
+        player_tags = re.findall("<(.*)>", summary_message)
+        
+        for players in player_tags:
+            player_id = int(players[1:])
+
+            user = await guild.fetch_member(player_id)
+            nickname = user.nick
+            
+            character_name = nickname.split('|')[1].strip()
+            player_name = nickname.split('|')[2].strip()
+
+
+            print(f'{character_name} : {player_name}')
+
+
+
+    
+    print('All messages are done, pls close the code')
+    
+    
 
 
 # Run the bot
 client.run(TOKEN)
+
