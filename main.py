63cc80fa889ba3c7062f3d9983bcf15f22eef75b
Manasvi Sinha <sinha5432>
2024-05-06 16:07:48 +0530

    made the code working

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-
-# 6fadce0fbe0d3be98e6fef69b03194b24abb982592438d3c037b61c5f917bdb7e618ac4dd36f4ada334e078600c30756516fe5769a0a2b514b4d417f1bb75266d0fba1c1a0d61677d948aa3d75051cbe17b2bf497fdf36180363f78899e114d5ad0a12bed55aee2c8b3e49f521665a1fdebbe844e25f8aed4db0afa32eb4803442fbf0c97b25489d9cf4721818af3be2821fbdb4ab7b3925857188fce7d26f8d35
-
 import discord
+# permission -integer: 68608
+# Discord bot token
+TOKEN = 'MTIzNjk3MzIzNjcxODE0MTUyMg.GQYctX.IftPkFEHXxQWM1HaW3QYdVd9-BBC5r4hJ-Ix14'
 
+# ID of the server and channel you want to read messages from
+SERVER_ID = 1219687915798663349
+CHANNEL_ID = 1219689307829440562
+
+# Message to display
+DISPLAY_MESSAGE = "Hello from the bot!"
+
+# Initialize the bot
 intents = discord.Intents.default()
-intents.messages = True
+intents.messages = True  # Enable the message intent
+intents.message_content = True # Enable message texts
+client = discord.Client(intents=intents)
 
-client = discord.Client(intents = intents)
-guild = discord.Guild
-
-
-print("Bot started")
 @client.event
-async def on_message(message):
-    
-    channelIDsToListen = ['bot-rolls'] # put the channels that you want to listen to here
+async def on_ready():
+    print(f'Logged in as {client.user}')
 
-    if message.channel.id in channelIDsToListen:
-
-        if message.content != "" :
-            print("New message: " + message.content)
+    messages = []
     
-client.run('6fadce0fbe0d3be98e6fef69b03194b24abb982592438d3c037b61c5f917bdb7e618ac4dd36f4ada334e078600c30756516fe5769a0a2b514b4d417f1bb75266d0fba1c1a0d61677d948aa3d75051cbe17b2bf497fdf36180363f78899e114d5ad0a12bed55aee2c8b3e49f521665a1fdebbe844e25f8aed4db0afa32eb4803442fbf0c97b25489d9cf4721818af3be2821fbdb4ab7b3925857188fce7d26f8d35')
+    channel = client.get_channel(CHANNEL_ID)
+
+    async for message in channel.history(limit=5):
+        messages.append(message)
+
+    # Display another message after reading all the previous messages
+    # await channel.send(DISPLAY_MESSAGE)
+
+ 
+
+    # Print all the previous messages
+
+    for message in messages:
+        print(f"{message.author}: {message.content}")
+
+
+# Run the bot
+client.run(TOKEN)
