0c27352b21164c1921060776e959b953d19fed85
Manasvi Sinha <sinha5432>
2024-05-09 21:38:09 +0530

    added level column and its logic

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ TOKEN = 't'
 SERVER_ID = 1219687915798663349
 CHANNEL_ID = 1219689307829440562
 
-# Message to display
-DISPLAY_MESSAGE = "Hello from the bot!"
+MESSAGES_TO_FETCH = 1
 
 # Initialize the bot
 intents = discord.Intents.default()
@@ -36,22 +35,47 @@ sheet = google_client.open('Tales of Exandria: Character levels').sheet1
 
 data = sheet.get_all_records()
 levels_df = pd.DataFrame(data)
-print(levels_df)
+
+def calculate_lvl(games_played: int) -> int:
+    '''
+    Level to get to   Games to play    Total
+     2              2                2
+     3              3                5
+     4              5                10
+     5              5                15
+     6              10               25
+    '''
+
+    print(games_played)
+    print(type(games_played))
+
+    if games_played >=23:
+        return 6
+
+    if games_played >= 13:
+        return 5
+
+    if games_played >= 8:
+        return 4
+    
+    if games_played >= 3:
+        return 3
+    
+    return 2
+    
+
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'INITIALISING AS {client.user}')
 
     messages = []
     
     channel = client.get_channel(CHANNEL_ID)
 
-    async for message in channel.history(limit=5):
+    async for message in channel.history(limit=MESSAGES_TO_FETCH):
         messages.append(message)
 
-    # Display another message after reading all the previous messages
-    # await channel.send(DISPLAY_MESSAGE)
-    
 
     guild = await client.fetch_guild(SERVER_ID)
 
@@ -66,13 +90,9 @@ async def on_ready():
         message_id = str(message.id)
 
         
-        
         if message_id not in ingested_msg:
-            print('not found')
+            print('NEW SUMMARY DETECTED, PARSING....')
             ingested_msg.append(message_id)
-
-            open("ingested_messages.txt", )
-
 
 
             player_tags = re.findall("<(.*)>", summary_message)
@@ -88,15 +108,18 @@ async def on_ready():
 
                 if character_name not in levels_df['Character'].to_list():
                     num_of_characters = len(levels_df.index)
-
                     levels_df.loc[num_of_characters] = [num_of_characters, character_name, player_name, 1]
                 else:
 
                     player_index = levels_df.index[levels_df.Character == character_name]
                     
-                    games_played = levels_df.loc[player_index, 'Games-Played' ].astype(int)
+                    games_played = levels_df.loc[player_index, 'Total-Games-Played' ].astype(int).iloc[0]
 
-                    levels_df.loc[player_index, 'Games-Played' ] = games_played + 1
+                    current_lvl = calculate_lvl(games_played)
+
+                    levels_df.loc[player_index, 'Total-Games-Played' ] = games_played + 1
+
+                    levels_df.loc[player_index, 'Current-Level' ] = current_lvl
                     
                     sheet.update([levels_df.columns.values.tolist()] + levels_df.values.tolist())
                 
